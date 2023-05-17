fb873c33bf95fa4089a21fc94d3bfda8b018797a
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-05-17 21:07:59 +0530

    day 3

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "../src/styles.scss"
+import './App.css'
+import Board from "./components/Board"
+
+function App() {
+ 
+
+  return (
+  <div className="app">
+   <Board/>
+  </div>
+  )
+}
+
+export default App
