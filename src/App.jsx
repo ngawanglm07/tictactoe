4aece4c19997c5bd69bc2d333491663329b0beb6
Ngawanglm <109843746+ngawanglm07@users.noreply.github.com>
2023-05-18 20:10:06 +0530

    day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import "../src/styles.scss"
 import './App.css'
 import Board from "./components/Board"
+import React, { useState } from 'react'
 
 function App() {
- 
+  const [squares , setSquares] = useState(Array(9).fill(null));
+  const [ isXNext , setisXNext] = useState(false);
+
+  const nextPlayer = isXNext ? 'X' : 'O';
+
+  const handleSquareClick =  clickedPosition => {
+
+    if(squares[clickedPosition]) {
+      return ;
+    }
+     
+  setSquares(currentSquares => {
+    return currentSquares.map((squareValue , position) => {
+        if(clickedPosition === position){
+            return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+    })
+    
+  });
+  setisXNext((currentisXnext) => !currentisXnext);
+
+  }
+
 
   return (
   <div className="app">
-   <Board/>
+  <h2>the next player is {nextPlayer} </h2>
+   <Board squares={squares} handleSquareClick={handleSquareClick}/>
   </div>
   )
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,40 +1,13 @@
-import React, { useState } from 'react'
+
 import Square from './Square'
 
-const Board = () => {
-
-  const [squares , setSquares] = useState(Array(9).fill(null));
-  const [ isXNext , setisXNext] = useState(false);
-
-  
-
-  const handleSquareClick =  clickedPosition => {
-
-    if(squares[clickedPosition]) {
-      return ;
-    }
-     
-  setSquares(currentSquares => {
-    return currentSquares.map((squareValue , position) => {
-        if(clickedPosition === position){
-            return isXNext ? 'X' : 'O';
-        }
-        return squareValue;
-    })
-    
-  });
-  setisXNext((currentisXnext)=> !currentisXnext);
-
-  }
-
-
+const Board = ({squares, handleSquareClick}) => {
   const renderSquare = position => {
      return ( 
      <Square value={squares[position]}
       onClick={ () => handleSquareClick(position)}/>
        )
   }
-
 
   return (
     <div className='board'>
